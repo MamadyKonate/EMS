af47ad9d577bed3c62c9379105ac4cc5f1ec1ba8
=DocMilo <docmilo@users.noreply.github.com>
2023-05-04 11:39:19 +0100

    add initial repo files

---
+++ Logline/LogLine.cs
@@ -0,0 +1,58 @@
+﻿namespace Logline
+{
+    public class LogLine
+    {
+        /// <summary>
+        /// The Message method should return a log line's message. 
+        /// Example input: "[ERROR]: Invaid operation"
+        /// Example output: "Invald operation"
+        /// </summary>
+        /// <param name="logLine">The log message</param>
+        /// <returns>A string containing just the message element of the log</returns>
+        public static string Message(string logLine)
+        {
+            // [InlineData("[WARNING]:   \tTimezone not set  \r\n", "Timezone not set")]
+            // TODO - Implement the method
+            //throw new NotImplementedException("Implement the Message function to return the message string");
+
+            // Remove any 
+            string input = logLine.Replace("\n", "");
+            input = input.Replace("\t", "");
+            input = input.Replace("\r", "");
+            input = input.TrimEnd();
+            // Just get the text after the log
+            int endOfLog = input.IndexOf(":");
+            input = input.Substring(endOfLog + 1, input.Length - endOfLog - 1);
+
+            return input.TrimStart();
+        }
+
+        /// <summary>
+        /// The LogLevel method should return a log line's log level, in lower case.
+        /// Example input: "[ERROR]: Invaid operation"
+        /// Example output: "error"
+        /// </summary>
+        /// <param name="logLine"></param>
+        /// <returns>A lowercase string containing the log level</returns>
+        public static string LogLevel(string logLine)
+        {
+            //throw new NotImplementedException("Implement the LogLevel method to return the Log level");
+            int indexOf = logLine.IndexOf("]");
+            return logLine.Substring(1, indexOf - 1).ToLower();
+        }
+
+        /// <summary>
+        /// The Reformat method should reformat the log line, putting the message first and the 
+        /// log level after it in parentheses. The log level should be lowercase.
+        /// Example input: "[INFO]: operation completed"
+        /// Example output: "Operation completed (info)"
+        /// </summary>
+        /// <param name="logLine">The log message</param>
+        /// <returns></returns>
+        public static string Reformat(string logLine)
+        {
+            string returnString = Message(logLine) + " (" + LogLevel(logLine) + ")";
+            return returnString;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # dotnetBuild
-A sample repository that demonstrates building and testing a .net application 
+## Repository description
+A sample repository that demonstrates building and testing a C# .NET application using a github actions workflow. There are two GitHub actions workflows:  
+
+*  [dotnet.yml](.github/workflows/dotnet.yml) builds and tests the sample project using [dotnet build CLI](https://learn.microsoft.com/.en-us/dotnet/core/tools/)
+*  [superlinter.yml](.github/workflows/superlinter.yml)  lints the code in the repository using the GitHub action [Super-Linter](https://github.com/marketplace/actions/super-linter).  
+
+Note: The [dotnet.yml](.github/workflows/dotnet.yml) workflow explicitly calls the dotnet restore command, but in most cases you don't need to explicitly do this, as it is implicitly called by build, test, publish and restore. I call it in this workflow to demonstrate the --no-restore and --no-build options for build and test respectively.
+
+### Additional tasks to explore 
+There are a number of modifications that you could make to the sample here and I encourage you to explore the dotnet CLI [documentation](https://learn.microsoft.com/en-us/dotnet/core/tools/) further. You should consider trying to: 
+
+* Break the build and test into separate jobs with test depending on a successful build job completing. 
+* Build different configurations (release and debug).
+* Output and upload [test](https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-test) logs (trx and html formats supported).
+* If the buld and test jobs are successful then use the CLI dotnet [publish](https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-publish) command to publish a release build for deployment or hosting. 
+* If your own project builds a library you could use the CLI dotnet [pack](https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-pack)  
+
+
+I recommend that you fork the repo and try the dotnet CLI at the command line locally on your PC first before modifying the workflow.  
+
+Most of all __*play around and have fun*__.
